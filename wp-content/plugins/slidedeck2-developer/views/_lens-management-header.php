39f0f27c57d366a8a6d213705ce19ec4407d13a3
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-01-21 23:30:36 -0500

    remove a bunch of plugins

--- wp-content/plugins/slidedeck2-developer/views/_lens-management-header.php
+++
@@ -1,1 +0,0 @@
-<a class="button<?php if( $is_writable->valid !== true ) echo ' disabled' ?>" href="<?php echo slidedeck2_action( "/lenses&action=copy&lens=proto&new_lens_name=My%20Lens&new_lens_slug=my-lens&create_or_copy=create" ); ?>">Create New</a>
--- wp-content/plugins/wp-e-commerce/wpsc-admin/users.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-add_action( 'user_profile_update_errors', '_wpsc_action_user_update_errors', 10, 3 );
-
-function _wpsc_action_user_update_errors( $errors, $update, $user ) {
-	if ( $user->role == 'wpsc_anonymous' )
-		unset( $errors->errors['empty_email'] );
-}
