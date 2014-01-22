3c73de510a2d9928c15eeafd3c95ea75b6778e52
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-01-22 15:13:38 -0500

    adding shortcodes to handle the crazy styling

--- wp-content/themes/something-blue/lib/inc/shortcodes.php
+++ wp-content/themes/something-blue/lib/inc/shortcodes.php
@@ -1,4 +1,19 @@
 <?php
+add_shortcode('product-sidebar','product_sidebar_function');
+function product_sidebar_function(){
+    $ret = '<div class="product-sidebar pull-right col-md-4">';
+    $ret .= wp_nav_menu(array('menu'=>'Shirt Tools'));
+    $ret .= '</div>';
+    return $ret;
+}
+add_filter('product_images_box_filters','do_shortcode');
+add_shortcode('product-images-box', 'product_images_box_function');
+function product_images_box_function($atts,$content){
+    $ret = '<div class="product-images-box container"><div class="row">';
+    $ret .= apply_filters('product_images_box_filters',$content);
+    $ret .= '</div></div>';
+    return $ret;
+}
 add_shortcode('button','msdlab_button_function');
 function msdlab_button_function($atts, $content = null){	
 	extract( shortcode_atts( array(
