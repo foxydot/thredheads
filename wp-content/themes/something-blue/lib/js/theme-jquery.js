494c8896594ec7618ec6ee9cb08061f6e0cf24a6
MSDLAB <catherine@msdlab.com>
2014-03-07 15:01:13 -0700

    why did i ftp?

--- wp-content/themes/something-blue/lib/js/theme-jquery.js
+++ wp-content/themes/something-blue/lib/js/theme-jquery.js
@@ -1,24 +1,8 @@
 jQuery(document).ready(function($) {	
-	$('ul li:first-child').addClass('first-child');
-	$('ul li:last-child').addClass('last-child');
-	$('ul li:nth-child(even)').addClass('even');
-	$('ul li:nth-child(odd)').addClass('odd');
-	$('table tr:first-child').addClass('first-child');
-	$('table tr:last-child').addClass('last-child');
-	$('table tr:nth-child(even)').addClass('even');
-	$('table tr:nth-child(odd)').addClass('odd');
-	$('tr td:first-child').addClass('first-child');
-	$('tr td:last-child').addClass('last-child');
-	$('tr td:nth-child(even)').addClass('even');
-	$('tr td:nth-child(odd)').addClass('odd');
-    $('div:first-child').addClass('first-child');
-    $('div:last-child').addClass('last-child');
-    $('div:nth-child(even)').addClass('even');
-    $('div:nth-child(odd)').addClass('odd');
-    $('section:first-child').addClass('first-child');
-    $('section:last-child').addClass('last-child');
-    $('section:nth-child(even)').addClass('even');
-    $('section:nth-child(odd)').addClass('odd');
+	$('*:first-child').addClass('first-child');
+	$('*:last-child').addClass('last-child');
+	$('*:nth-child(even)').addClass('even');
+	$('*:nth-child(odd)').addClass('odd');
 
 
 	$('#footer-widgets div.widget:first-child').addClass('first-child');
